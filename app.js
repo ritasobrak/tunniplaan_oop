4858316c4a998781dcd7221057c88eab508dae0c
ritasobrak <margarita.sobrak@khk.ee>
2021-01-04 13:15:59 +0000

    Tunniplaani kuupaevade andmed on koostatud ja valjastatud

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 //tunniplaani objekt
 const tunniplaan = new Tunniplaan(346, '2021-01-04');
 
+//kasutaja liidese objekt
+const kl = new KasutajaLiides();
+
 // tunnilaani valjastamine
 document.addEventListener('DOMContentLoaded', valjastaTunniplaan);
 
 function valjastaTunniplaan() {
     tunniplaan.opetajaTunniplaaniAndmed().then(tunnid => {
-        console.log(tunnid);
-    }).catch(error => console.log(error));
+        kl.printNadalaKuupaevad(kl.nadalaKuupaevad(tunnid));
+    }).catch(viga => console.log(viga));
 }
